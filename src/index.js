8ce34804c1c41da94632d24647408e56e6843f3f
hugosmoreira <hugo.n4@gmail.com>
2021-08-27 18:54:59 -0700

    nucamp website

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "typeface-lobster";
+import "typeface-open-sans";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
